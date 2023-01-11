54bfd721826df457766a436c0e41e4e008860ffc
Marek Chalupa <mchqwerty@gmail.com>
2023-01-11 23:50:19 +0100

    Remove old files

--- runprimes-good.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cat primes.txt | ./regex /primes1 prime "([0-9]+)\s*([0-9]+)" dd&
-cat primes.txt | ./regex /primes2 prime "([0-9]+)\s*([0-9]+)" dd&
-
-wait
-wait
--- sendaddr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd $(dirname $0)
-
-./sendaddr /addr1 $1&
-./sendaddr /addr2 $1&
-
-wait
-wait
