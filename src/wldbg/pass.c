3597d79f5e524134b86c6710cec67bfe6d5c0c51
Marek Chalupa <mchqwerty@gmail.com>
2023-10-26 14:12:03 +0200

    wldbg: send pointer events

--- src/wldbg/pass.c
+++ src/wldbg/pass.c
@@ -35,173 +35,361 @@
  * SOFTWARE.
  */
 
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
+#include <string.h>
 
 #include <wayland-client-protocol.h>
 
-#include <wldbg.h>
-#include "wldbg-pass.h"
 #include "wldbg-parse-message.h"
+#include "wldbg-pass.h"
+#include <wldbg.h>
 
-static void
-send_event(struct wldbg_message *message);
+#include "vamos-buffers/core/event.h"
+#include "vamos-buffers/core/signatures.h"
+#include "vamos-buffers/core/source.h"
+#include "vamos-buffers/shmbuf/buffer.h"
+#include "vamos-buffers/shmbuf/client.h"
 
-static void
-handle_keyboard_message(struct wldbg_resolved_message *rm);
+static void send_event(struct wldbg_message *message);
 
-static void
-handle_pointer_message(struct wldbg_resolved_message *rm);
-
-struct vamos_data {
-	unsigned int incoming_number;
-	unsigned int outcoming_number;
-	unsigned int in_trasfered;
-	unsigned int out_trasfered;
+struct pass_data {
+  unsigned int incoming_number;
+  unsigned int outcoming_number;
+  unsigned int in_trasfered;
+  unsigned int out_trasfered;
 };
 
-static int
-vamos_in(void *user_data, struct wldbg_message *message)
-{
-	struct vamos_data *data = user_data;
-
-	data->incoming_number++;
-	data->in_trasfered += message->size;
-
-	/*
-	printf("GOT incoming message: %u, size: %lu bytes\n",
-	       data->incoming_number, message->size);
-	       */
-
-	send_event(message);
-
-	return PASS_NEXT;
-}
-
-static int
-vamos_out(void *user_data, struct wldbg_message *message)
-{
-	struct vamos_data *data = user_data;
-
-	data->outcoming_number++;
-	data->out_trasfered += message->size;
-
-	/*
-	printf("GOT outcoming message: %u, size: %lu bytes\n",
-	       data->outcoming_number, message->size);
-	       */
-
-	send_event(message);
-
-	return PASS_NEXT;
-}
-
-static void
-vamos_help(void *user_data)
-{
-	(void) user_data;
-}
-
-static int
-vamos_init(struct wldbg *wldbg, struct wldbg_pass *pass,
-	     int argc, const char *argv[])
-{
-	int i;
-	struct vamos_data *data = calloc(1, sizeof *data);
-	if (!data)
-		return -1;
-
-	(void) wldbg;
-
-	printf("-- Initializing VAMOS pass --\n\n");
-	for (i = 0; i < argc; ++i)
-		printf("\targument[%d]: %s\n", i, argv[i]);
-
-	printf("\n\n");
-
-	pass->user_data = data;
-
-	return 0;
-}
-
-static void
-vamos_destroy(void *user_data)
-{
-	struct vamos_data *data = user_data;
-
-	printf(" -- Destroying vamos pass --\n\n");
-	printf("Totally trasfered %u bytes from client to server\n"
-	       "and %u bytes from server to client\n",
-	       data->out_trasfered, data->in_trasfered);
-
-	free(data);
-}
-
-struct wldbg_pass wldbg_pass = {
-	.init = vamos_init,
-	.destroy = vamos_destroy,
-	.server_pass = vamos_in,
-	.client_pass = vamos_out,
-	.help = vamos_help,
-	.description = "Wldbg pass that forwards data into VAMOS"
+struct event {
+  vms_event base;
+  unsigned char args[];
 };
 
-void
-send_event(struct wldbg_message *message)
-{
-	int is_buggy = 0;
-	uint32_t pos;
-	struct wldbg_connection *conn = message->connection;
-	struct wldbg_resolved_message rm;
+vms_shm_buffer *top_buffer;
+vms_kind client_new_kind;
+vms_kind client_exit_kind;
+vms_eventid top_buffer_eventid;
 
-	if (!wldbg_resolve_message(message, &rm)) {
-		fprintf(stderr, "Failed resolving message, event lost...\n");
-		return;
-	}
+static size_t waiting_for_buffer = 0;
 
-	if (rm.wl_interface == &wl_pointer_interface) {
-		printf("%c: ", message->from == SERVER ? 'S' : 'C');
-		wldbg_message_print(message);
-		handle_pointer_message(&rm);
-	}
+enum vamos_event_idx {
+  POINTER_MOTION = 0,
+  KEYBOARD_KEY = 1,
+};
 
-	if (rm.wl_interface == &wl_keyboard_interface) {
-		printf("%c: ", message->from == SERVER ? 'S' : 'C');
-		wldbg_message_print(message);
-		handle_keyboard_message(&rm);
-	}
+struct kind_mapping {
+  const char *name;
+  const char *sig;
+  vms_kind kind;
+} vamos_events[] = {
+    /* the order of events must match the indices
+     * in the enum vamos_event_idx */
+    {"pointer_motion", "iii", 0},
+    {"keyboard_key", "iiii", 0},
+};
 
-	return;
+struct connection_data {
+  vms_eventid next_id;
+  vms_shm_buffer *buffer;
+  size_t waiting_for_buffer;
 
+  struct kind_mapping events[sizeof vamos_events / sizeof vamos_events[0]];
+};
+
+static void handle_keyboard_message(struct wldbg_resolved_message *rm,
+                                    struct connection_data *data);
+
+static void handle_pointer_message(struct wldbg_resolved_message *rm,
+                                   struct connection_data *data);
+
+static unsigned char *data_ptr(struct connection_data *data) {
+  unsigned char *addr;
+  while (!(addr = vms_shm_buffer_start_push(data->buffer))) {
+    ++data->waiting_for_buffer;
+  }
+
+  return addr;
 }
 
-void
-handle_keyboard_message(struct wldbg_resolved_message *rm)
-{
-	unsigned int pos = 0;
-	const struct wl_message *wl_message = rm->wl_message;
-	struct wldbg_resolved_arg *arg;
+struct vms_source_control *sub_control;
 
-	printf("event/request: %s\n", rm->wl_message->name);
-	while((arg = wldbg_resolved_message_next_argument(rm))) {
-		assert(arg != NULL);
-		printf("  pos=%u, p=%u\n", pos, arg->data ? *((uint32_t *)arg->data) : 0);
-		++pos;
-	}
+static vms_shm_buffer *init_vamos(const char *shmkey) {
+
+  struct vms_source_control *top_control =
+      vms_source_control_define(2, "client_new", "l", "client_exit", "l");
+  if (!top_control) {
+    fprintf(stderr, "%s:%d: Failed defining source control\n", __FILE__,
+            __LINE__);
+    return NULL;
+  }
+
+  sub_control = vms_source_control_define(
+      sizeof vamos_events / sizeof vamos_events[0],
+      vamos_events[POINTER_MOTION].name, vamos_events[POINTER_MOTION].sig,
+      vamos_events[KEYBOARD_KEY].name, vamos_events[KEYBOARD_KEY].sig);
+  if (!sub_control) {
+    fprintf(stderr, "%s:%d: Failed defining source control\n", __FILE__,
+            __LINE__);
+    free(top_control);
+    return NULL;
+  }
+
+  const size_t capacity = 128;
+  top_buffer = vms_shm_buffer_create(shmkey, capacity, top_control);
+  if (!top_buffer) {
+    fprintf(stderr, "Failed creating shm buffer\n");
+    free(top_control);
+    free(sub_control);
+    return NULL;
+  }
+
+  fprintf(stderr, "info: waiting for the monitor to attach... ");
+  if (vms_shm_buffer_wait_for_reader(top_buffer) < 0) {
+    fprintf(stderr, "Failed waiting for the monitor to attach...\n");
+    vms_shm_buffer_destroy(top_buffer);
+    free(top_control);
+    free(sub_control);
+    return NULL;
+  }
+  fprintf(stderr, "done\n");
+
+  size_t events_num;
+  struct vms_event_record *events =
+      vms_shm_buffer_get_avail_events(top_buffer, &events_num);
+
+  struct vms_event_record *event = events;
+  for (int i = 0; i < events_num; ++i) {
+    printf("event-kind kind: %lu\n", event->kind);
+    if (strcmp(event->name, "client_new") == 0) {
+      client_new_kind = event->kind;
+    } else if (strcmp(event->name, "client_exit") == 0) {
+      client_exit_kind = event->kind;
+    }
+    ++event;
+  }
+
+  return top_buffer;
 }
 
-void
-handle_pointer_message(struct wldbg_resolved_message *rm)
-{
-	unsigned int pos = 0;
-	const struct wl_message *wl_message = rm->wl_message;
-	struct wldbg_resolved_arg *arg;
+static void destroy_data(struct wldbg_connection *conn, void *data) {
+  struct connection_data *cdata = (struct connection_data *)data;
+  vms_shm_buffer_destroy_sub_buffer(cdata->buffer);
+  free(data);
+}
 
-	printf("event/request: %s\n", rm->wl_message->name);
-	while((arg = wldbg_resolved_message_next_argument(rm))) {
-		assert(arg != NULL);
-		printf("  pos=%u, p=%u\n", pos, arg->data ? *((uint32_t *)arg->data) : 0);
-		++pos;
-	}
+struct connection_data *get_connection_data(struct wldbg_message *msg) {
+  struct connection_data *data =
+      wldbg_connection_get_user_data(msg->connection);
+  if (data) {
+    return data;
+  }
+
+  printf("CREATING SUBBUFFER\n");
+
+  /* TODO top_buffer send new client found */
+  assert(top_buffer && "No top-level SHM buffer");
+  assert(sub_control);
+  const size_t capacity = 100;
+  vms_shm_buffer *buffer =
+      vms_shm_buffer_create_sub_buffer(top_buffer, capacity, sub_control);
+
+  if (!buffer) {
+    fprintf(stderr, "Failed creating shm buffer\n");
+    return NULL;
+  }
+
+  fprintf(stderr, "info: waiting for the monitor to attach... ");
+  if (vms_shm_buffer_wait_for_reader(buffer) < 0) {
+    fprintf(stderr, "Failed waiting for the monitor to attach...\n");
+    vms_shm_buffer_destroy(buffer);
+    return NULL;
+  }
+  fprintf(stderr, "done\n");
+
+  data = calloc(1, sizeof *data);
+  if (!data) {
+    fprintf(stderr, "Failed memory allocation");
+    abort();
+  }
+
+  data->buffer = buffer;
+
+  size_t events_num;
+  struct vms_event_record *events =
+      vms_shm_buffer_get_avail_events(buffer, &events_num);
+
+  struct vms_event_record *event = events;
+  for (int i = 0; i < events_num; ++i) {
+    printf("event-kind kind: %lu\n", event->kind);
+    if (strcmp(event->name, vamos_events[POINTER_MOTION].name) == 0) {
+      data->events[POINTER_MOTION].kind = event->kind;
+      printf("POINTER_MOTION kind: %lu\n", event->kind);
+    } else if (strcmp(event->name, vamos_events[KEYBOARD_KEY].name) == 0) {
+      data->events[KEYBOARD_KEY].kind = event->kind;
+      printf("KEYBOARD_KEY kind: %lu\n", event->kind);
+    }
+    ++event;
+  }
+
+  wldbg_connection_set_user_data(msg->connection, data, destroy_data);
+
+  return data;
+}
+
+static void help(void *user_data) { (void)user_data; }
+
+static int init(struct wldbg *wldbg, struct wldbg_pass *pass, int argc,
+                const char *argv[]) {
+  (void)wldbg;
+
+  printf("-- Initializing VAMOS pass --\n\n");
+  for (int i = 0; i < argc; ++i)
+    printf("\targument[%d]: %s\n", i, argv[i]);
+
+  printf("\n\n");
+
+  if (!init_vamos(argv[1])) {
+    fprintf(stderr, "Failed initializing VAMOS");
+    return -1;
+  }
+
+  struct pass_data *data = calloc(1, sizeof *data);
+  if (!data) {
+    fprintf(stderr, "Memory allocation failed");
+    return -1;
+  }
+
+  pass->user_data = data;
+
+  return 0;
+}
+
+static void destroy(void *user_data) {
+  struct pass_data *data = user_data;
+
+  printf(" -- Destroying vamos pass --\n\n");
+  printf("Totally trasfered %u bytes from client to server\n"
+         "and %u bytes from server to client\n",
+         data->out_trasfered, data->in_trasfered);
+
+  free(data);
+  free(sub_control);
+
+  vms_shm_buffer_destroy(top_buffer);
+}
+
+static int message_in(void *user_data, struct wldbg_message *message) {
+  struct pass_data *data = user_data;
+
+  data->incoming_number++;
+  data->in_trasfered += message->size;
+
+  /*
+  printf("GOT incoming message: %u, size: %lu bytes\n",
+         data->incoming_number, message->size);
+         */
+
+  send_event(message);
+
+  return PASS_NEXT;
+}
+
+static int message_out(void *user_data, struct wldbg_message *message) {
+  struct pass_data *data = user_data;
+
+  data->outcoming_number++;
+  data->out_trasfered += message->size;
+
+  /*
+  printf("GOT outcoming message: %u, size: %lu bytes\n",
+         data->outcoming_number, message->size);
+         */
+
+  send_event(message);
+
+  return PASS_NEXT;
+}
+
+struct wldbg_pass wldbg_pass = {.init = init,
+                                .destroy = destroy,
+                                .server_pass = message_in,
+                                .client_pass = message_out,
+                                .help = help,
+                                .description =
+                                    "Wldbg pass that sends data into VAMOS"};
+
+void send_event(struct wldbg_message *message) {
+  int is_buggy = 0;
+  uint32_t pos;
+  struct wldbg_connection *conn = message->connection;
+  struct wldbg_resolved_message rm;
+
+  struct connection_data *data = get_connection_data(message);
+
+  if (!wldbg_resolve_message(message, &rm)) {
+    // fprintf(stderr, "Failed resolving message, event lost...\n");
+    return;
+  }
+
+  if (rm.wl_interface == &wl_pointer_interface) {
+    printf("%c: ", message->from == SERVER ? 'S' : 'C');
+    wldbg_message_print(message);
+    handle_pointer_message(&rm, data);
+  }
+
+  if (rm.wl_interface == &wl_keyboard_interface) {
+    printf("%c: ", message->from == SERVER ? 'S' : 'C');
+    wldbg_message_print(message);
+    handle_keyboard_message(&rm, data);
+  }
+
+  return;
+}
+
+void handle_keyboard_message(struct wldbg_resolved_message *rm,
+                             struct connection_data *data) {
+  unsigned int pos = 0;
+  const struct wl_message *wl_message = rm->wl_message;
+  struct wldbg_resolved_arg *arg;
+
+  /*
+  printf("event/request: %s\n", rm->wl_message->name);
+  while((arg = wldbg_resolved_message_next_argument(rm))) {
+          assert(arg != NULL);
+          printf("  pos=%u, p=%u\n", pos, arg->data ? *((uint32_t *)arg->data) :
+  0);
+          ++pos;
+  }
+  */
+}
+
+void handle_pointer_message(struct wldbg_resolved_message *rm,
+                            struct connection_data *data) {
+  unsigned int pos = 0;
+  const struct wl_message *wl_message = rm->wl_message;
+  struct wldbg_resolved_arg *arg;
+
+  if (strcmp(wl_message->name, "motion") == 0) {
+    unsigned char *addr = data_ptr(data);
+    ++data->next_id;
+    printf("SEND: %lu of %lu\n", data->next_id,
+           data->events[POINTER_MOTION].kind);
+    addr = vms_shm_buffer_partial_push(data->buffer, addr,
+                                       &data->events[POINTER_MOTION].kind,
+                                       sizeof(vms_kind));
+    addr = vms_shm_buffer_partial_push(data->buffer, addr, &data->next_id,
+                                       sizeof(vms_eventid));
+    vms_shm_buffer_finish_push(data->buffer);
+  }
+
+  /*
+  printf("event/request: %s\n", rm->wl_message->name);
+  while((arg = wldbg_resolved_message_next_argument(rm))) {
+          assert(arg != NULL);
+          printf("  pos=%u, p=%u\n", pos, arg->data ? *((uint32_t *)arg->data) :
+  0);
+          ++pos;
+  }
+  */
 }
